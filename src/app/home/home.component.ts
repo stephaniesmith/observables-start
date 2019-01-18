58269fe63319b0717ae4ba32394bd3c17d80ec46
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-01-18 08:14:35 -0800

    unsubscribing to a observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { interval, Observable, Observer } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Observable, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    // const myNumbers = interval(1000);
-    // myNumbers.subscribe((number: number) => {
-    //   console.log(number);
-    // });
+    const myNumbers = interval(1000);
+    this.numbersObsSubscription = myNumbers.subscribe((number: number) => {
+      console.log(number);
+    });
 
     const myObservable = Observable.create((observer: Observer<string>) => {
       setTimeout(() => {
@@ -32,7 +34,7 @@ export class HomeComponent implements OnInit {
       }, 6000);
     });
 
-    myObservable.subscribe(
+    this.customObsSubscription = myObservable.subscribe(
       (data: string) => {
         console.log(data);
       },
@@ -43,6 +45,11 @@ export class HomeComponent implements OnInit {
         console.log('completed');
       },
     );
+  }
+
+  ngOnDestroy() {
+    this.numbersObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
   }
 
 }
