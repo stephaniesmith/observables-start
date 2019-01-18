98074d21521bb5d41287efa0dd0fcd8641b1a74b
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-01-18 08:29:10 -0800

    observable operators

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { interval, Observable, Observer, Subscription } from 'rxjs';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +14,10 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = interval(1000);
+    const myNumbers = interval(1000)
+      .map((data: number) => {
+        return data * 2;
+      });
     this.numbersObsSubscription = myNumbers.subscribe((number: number) => {
       console.log(number);
     });
