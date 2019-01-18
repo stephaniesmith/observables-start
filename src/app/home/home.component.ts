bd0c942632634791bf1bad31dc36306e88b5ef46
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-01-18 08:08:59 -0800

    learning about observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,38 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // const myNumbers = interval(1000);
+    // myNumbers.subscribe((number: number) => {
+    //   console.log(number);
+    // });
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second');
+      }, 4000);
+      setTimeout(() => {
+        // observer.next('this does not work');
+        observer.complete();
+      }, 5000);
+      setTimeout(() => {
+        observer.next('third');
+      }, 6000);
+    });
+
+    myObservable.subscribe(
+      (data: string) => {
+        console.log(data);
+      },
+      (error: string) => {
+        console.log(error);
+      },
+      () => {
+        console.log('completed');
+      },
+    );
   }
 
 }
